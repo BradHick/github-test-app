92f05ec7a13d14ea19cd7a7261fdb614bb6fc583
BradHick <bradleyhrm@gmail.com>
2019-05-28 23:43:48 -0300

    feat: create repo page

---
+++ src/scenes/repository/container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+
+const mapStateToProps = ({ user, repo }) => ({
+  repos: repo.repos,
+  user: user.user,
+  loading: repo.loading,
+});
+
+const mapDispatchToProps = ({ user, repo, commit }) => ({
+  fetchUser: user.fetchUser,
+  fetchRepos: repo.fetchRepos,
+  resetRepos: repo.resetRepos,
+  fetchCommits: commit.fetchCommits
+});
+
+export default component => connect(mapStateToProps, mapDispatchToProps)(component);
---
+++ src/scenes/repository/index.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+// import differenceInDays from 'date-fns/difference_in_days';
+import { Link } from 'react-router-dom';
+import { Formik } from 'formik';
+import { getTimeRange, sortRepos } from '../../helpers';
+
+// Container
+import container from './container';
+
+// Components
+import {
+  If,
+  Card,
+  Info,
+  Avatar,
+  Loading,
+  Select,
+  Button,
+  Form,
+  BackPage,
+  Preloader,
+  Repo
+} from '../../components';
+
+
+
+const getReposList = (params, fetchRepos, fetchUser) => {
+  if (params.username) {
+    fetchRepos(params.username);
+    fetchUser(params.username);
+  }
+};
+
+class Repository extends Component {
+
+  state = {
+    sortedRepos: [],
+  };  
+
+  componentDidMount() {
+    const { fetchRepos, fetchUser, match: { params } } = this.props;
+    getReposList(params, fetchRepos, fetchUser);
+  }
+
+  componentWillUnmount(){
+    const { resetRepos } = this.props;
+    resetRepos();
+  }
+
+  render() {
+    const { repos, user, fetchCommits, loading } = this.props;
+    return (
+      <>
+        <If condition={repos && repos.length}>
+          <Card info>
+            <div>
+              <Avatar src={user.avatar_url} />
+              <center><h2>{user.login}</h2></center>
+            </div>
+            
+            <Info
+              value={user ? user.following : null}
+              description={'following'}
+            />
+            <Info
+              value={user ? user.followers : null}
+              description={'followers'}
+            />
+            <Info
+              value={user ? user.public_repos : null}
+              description={'public repos'}
+            />
+            <Info
+              value={user ? user.public_gists : null}
+              description={'public gists'}
+            />
+          </Card>
+          
+          <Card>
+            <h2><center>Select filter</center></h2>
+            <Formik 
+              initialValues={{ field:'stargazers_count', order: 'desc'}}
+              onSubmit={values => {
+                this.setState({ sortedRepos: sortRepos(repos, values.field, values.order) })
+              }}
+
+              render={({ handleSubmit, handleChange }) => (
+                <Form onSubmit={handleSubmit}>
+                  <Select name='field' onChange={handleChange}>
+                    <option value='stargazers_count'>Stars</option>
+                    <option value='watchers_count'>Watchers</option>
+                    <option value='updated_at'>Updated at</option>
+                  </Select>
+                  <Select name='order' onChange={handleChange}>
+                    <option value='desc'>Desc</option>
+                    <option value='asc'>Asc</option>
+                  </Select>
+                  <Button type='submit' full>
+                    {'Sort'}
+                  </Button>
+
+                </Form>
+              )}
+            />
+            
+          </Card>
+          <BackPage to={'/'} />
+          { 
+            this.state.sortedRepos.length > 0 ?
+            this.state.sortedRepos.map(({ name, updated_at, description, watchers_count, stargazers_count }, i) => {
+            return (
+              <Card key={i} onClick={() => fetchCommits(user.login,name)}>
+                <Link
+                  to={`/commits-list/${user.login}/${name}`}
+                  style={{ textDecoration: 'none' }}
+                >
+                  <Repo
+                    name={name}
+                    description={description}
+                    commitStatus={getTimeRange(updated_at)}
+                    stars={stargazers_count}
+                    watchers={watchers_count}
+                  />
+                </Link>
+              </Card>
+            );
+            }) :
+            repos.map(({ name, updated_at, description, watchers_count, stargazers_count }, i) => {
+              return (
+                <Card key={i} onClick={() => fetchCommits(user.login,name)}>
+                  <Link
+                    to={`/commits-list/${user.login}/${name}`}
+                    style={{ textDecoration: 'none' }}
+                  >
+                    <Repo
+                      name={name}
+                      description={description}
+                      commitStatus={getTimeRange(updated_at)}
+                      stars={stargazers_count}
+                      watchers={watchers_count}
+                    />
+                  </Link>
+                </Card>
+              );
+              })
+          }
+        </If>
+        <Loading>
+          <If condition={loading}>
+            <Preloader />
+          </If>
+        </Loading>
+      </>
+    );
+  }
+}
+
+export default container(Repository);
