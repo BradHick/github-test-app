433118c51a5dcb0ea8f23c037f326a44bd6864b7
BradHick <bradleyhrm@gmail.com>
2019-05-30 00:27:26 -0300

    design(Card): add layout with responsivity

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,19 @@
 import styled from 'styled-components';
 
 const Card = styled.div`
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.08);
-  background-color: #ffffff;
+  background-color: ${props => (props.white ? '#ffffff' : '#d9d9d9')};
   min-height: 40px;
-  margin: 0 auto;
+  border-radius: 15px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
   padding: 20px;
   margin-top: 10px;
   text-decoration: none;
-  display: ${p => (p.info ? 'flex' : 'block')};
-  @media (max-width: 800px) { width: 80%; }
+  display: flex;
+  align-items: center;
+  width: 100%;
+  @media (max-width: 800px) {
+    width: 80%;
+  }
 `;
 
 export default Card;
