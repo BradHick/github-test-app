246612775833348af1cd0bd098790cdbfa37609f
BradHick <bradleyhrm@gmail.com>
2019-05-30 00:29:31 -0300

    design(Form): change layout with responsivity

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
 const Form = styled.form`
-display: ${p => p.flex ? 'flex': 'grid'};
-grid-template-columns: 38% 38% 20%;
-justify-items: center;
-grid-gap: 15px;
+  display: ${p => (p.flex ? 'flex' : 'grid')};
+  grid-template-columns: 38% 38% 20%;
+  justify-items: center;
+  grid-gap: 15px;
+  flex: 1;
+  align-items: center;
+  @media (max-width: 800px) {
+    grid-template-columns: 35% 35% 20%;
+  }
 `;
 
 export default Form;
