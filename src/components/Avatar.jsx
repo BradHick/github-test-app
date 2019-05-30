c6d7e1ffbca303763a6367ee5d19578b739d083d
BradHick <bradleyhrm@gmail.com>
2019-05-30 00:26:47 -0300

    design(Avatar): add layout with responsivity

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
 const Avatar = styled.img`
-  width: 150px;
-  height: 150px;
+  width: 80px;
+  height: 80px;
   display: block;
   border-radius: 50%;
-  box-shadow: 0 0 4px rgba(0,0,0,0.08);
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.08);
+  @media (max-width: 800px) {
+    width: 50px;
+    height: 50px;
+  }
 `;
 
 export default Avatar;
