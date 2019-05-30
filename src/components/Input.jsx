28c61892516823065ae19e3e477f144be0cde7db
BradHick <bradleyhrm@gmail.com>
2019-05-30 00:30:25 -0300

    design(Input): change layout

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,16 +5,13 @@ const InputField = styled.input`
   width: 100%;
   height: 36px;
   display: flex;
-  background-color: rgba(255,188,53,0);
-  border: 1px solid  #e9e9e9;
-  text-align: center;
-  font-family: 'Montserrat';
+  font-size: 15px;
+  font-family: 'Righteous', cursive;
   letter-spacing: 1px;
+  background-color: transparent;
+  border: none;
 `;
 
-
-const Input = ({ ...props }) => (
-    <InputField {...props} />
-);
+const Input = ({ ...props }) => <InputField {...props} />;
 
 export default Input;
