b703f07a4e48ddd12870027bda34e577ed8bea90
BradHick <bradleyhrm@gmail.com>
2019-05-30 00:28:51 -0300

    design(Container): change layout with responsivity

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,9 +2,15 @@ import styled from 'styled-components';
 import { SECONDARY_COLOR } from '../const';
 
 const Container = styled.section`
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
   min-height: -webkit-fill-available;
-  background-color: ${SECONDARY_COLOR};
-  padding: 5% 10%;
+  background-color: #343434;
+  padding: 20px 0 50px 0;
+  max-width: 500px;
 `;
 
 export default Container;
